ed9d7180c6c8cda261395cc35f220d974df93ec0
faixarica <faixaricaa@gmail.com>
2025-08-29 11:53:15 -0300

    fix dv.py

--- db.py
+++ db.py
@@ -8,3 +8,6 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL não encontrado...")
